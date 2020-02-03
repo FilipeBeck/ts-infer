b77eb6500c0a1ed402c077d2d495341a3a4fda67
Filipe Roberto Beck <filipe.beck@gmail.com>
2020-02-02 22:09:02 -0300

    Atualiza deploy da documentação - extrai informações de "package.json"

--- deploy-doc.sh
+++ deploy-doc.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 DOC_SRC_PATH="./doc"
 DOC_SRC_GLOB="$DOC_SRC_PATH/*"
 DOC_OUTPUT="./docs"
@@ -16,10 +15,36 @@ if [ -d $DOC_OUTPUT ]; then
 	rm -rf $DOC_OUTPUT
 fi
 
+PACKAGE_NAME="$(node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json', 'utf8')).name)")"
+PACKAGE_DESCRIPTION="$(node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json', 'utf8')).description)")"
+PACKAGE_DOCS="$(node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json', 'utf8')).docs)")"
+
+if [ -z "$PACKAGE_NAME" ] || [ "$PACKAGE_NAME" = "undefined" ]; then
+	echo "Campo \"name\" inexistente em \"package.json\""
+	exit 1
+fi
+
+if [ -z "$PACKAGE_DESCRIPTION" ] || [ "$PACKAGE_DESCRIPTION" = "undefined" ]; then
+	echo "Camppo \"description\" inexistente em \"package.json\""
+	exit 1
+fi
+
+if [ -z "$PACKAGE_DOCS" ] || [ "$PACKAGE_DOCS" = "undefined" ]; then
+	echo "Campo \"docs\" inexistente em \"package.json\""
+	exit 1
+fi
+
 git clone $GIT_URL $DOC_OUTPUT --depth=1
 
 if [ ! $? ]; then
 	echo "Erro ao clonar repositório \"$GIT_URL\""
+fi
+
+DOCS_README="$DOC_OUTPUT/README.md"
+README_ITEM="$(grep $PACKAGE_NAME $DOCS_README)"
+
+if [ -z "$README_ITEM" ]; then
+	cat <<< "- [$PACKAGE_NAME](./$PACKAGE_NAME) - [$PACKAGE_DESCRIPTION]($PACKAGE_DOCS)$(\n)" >> $DOCS_README
 fi
 
 if [ ! -d $TS_INFER_PATH ]; then
@@ -27,7 +52,7 @@ if [ ! -d $TS_INFER_PATH ]; then
 fi
 
 cp -r $DOC_SRC_GLOB $TS_INFER_PATH &&
-cd $TS_INFER_PATH
+cd $DOC_OUTPUT
 
 if [ ! $? ]; then
 	echo "Erro ao copiar os arquivos de documentação"
